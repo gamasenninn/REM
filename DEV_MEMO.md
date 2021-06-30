9fe896b4727f7de9d6e11dd5084b194303e2d180
gamasenninn <gamasenninn@yahoo.co.jp>
2021-07-01 07:44:26 +0900

    sqltest.py complete

---
+++ DEV_MEMO.md
@@ -0,0 +1,6 @@
+# REM Developer's MEM
+
+
+21/07/01:
+とりあえず、sqlwrap_test.pyのテストをすべて実装。
+
--- sqlwrap_test.py
+++ sqlwrap_test.py
@@ -16,11 +16,18 @@ class TestSqlwrap(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        print("----setUpClass--------------")   
+        print("----setUp--------------")   
         self.conn = sqlite3.connect(':memory:')
 
+    @classmethod
+    def tearDownClass(self):
+        print("----tearDown-----------")
+        conn = self.conn
+        conn.commit()
+        conn.close()
 
-    def test_create_table(self):
+
+    def test_00_create_table(self):
         sql = '''
             CREATE TABLE "納品" (
                     "ID"	INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -39,7 +46,7 @@ class TestSqlwrap(unittest.TestCase):
         self.assertEqual('納品',ret[0][0])
 
 
-    def test_insert_dict(self):
+    def test_01_insert_dict(self):
         print("\n-----insert into Table by DICT------")
         conn = self.conn
         data = {'納品日': "2020/11/01",'納品先': "ABC商店",'担当者': "小野",'摘要': "特別注文"}
@@ -58,7 +65,7 @@ class TestSqlwrap(unittest.TestCase):
         print(t1,t2)
         self.assertEqual(t1,t2)
 
-    def test_insert_json(self):
+    def test_02_insert_json(self):
         print("\n-----insert into Table by JSON------")
         conn = self.conn
         data = {'納品日': "2020/11/02",'納品先': "ABC商店",'担当者': "小野",'摘要': "特別注文"}
@@ -78,7 +85,7 @@ class TestSqlwrap(unittest.TestCase):
         print(t1,t2)
         self.assertEqual(t1,t2)
 
-    def test_update_dict(self):
+    def test_03_update_dict(self):
         print("\n-----Update Table by DICT------")
         conn = self.conn
         data = {'ID': 2,'納品日': "2020/11/04",'納品先': "更新テスト",'担当者': "小野2",'摘要': "特別注文2"}
@@ -97,47 +104,45 @@ class TestSqlwrap(unittest.TestCase):
         print(t1,t2)
         self.assertEqual(t1,t2)
 
+    def test_04_son_select_all(self):       
+        print("\n-----select data JSON ------")
+        conn = self.conn
+        jdata = json_select_all(conn,'納品')
+        ret = json.loads(jdata)
+        print(ret)
+
+        c = conn.cursor()
+        c.execute('SELECT * FROM 納品' )
+        ret2 = c.fetchall()
+
+        print(ret2)
+        self.assertEqual(ret,ret2)
+        
+    def test_05_delete_dict(self):
+        print("\n-----delete data ------")
+        conn = self.conn
+        c = conn.cursor()
+        
+        c.execute('SELECT count(*)as RCOUNT FROM 納品' )
+        ret = c.fetchall()
+        print(ret)
+        rcount = ret[0]['RCOUNT']
+
+        
+        data = {'ID': 2}
+        ret = dict_delete(conn,'納品',data,'ID')
+        print (ret)
+
+        c.execute('SELECT count(*)as RCOUNT FROM 納品' )
+        ret = c.fetchall()
+        print(ret)
+        rcount2 = ret[0]['RCOUNT']
+
+        diff = rcount-rcount2
+        self.assertEqual(diff,1)
+        
 
 
 if __name__ == "__main__":
 
     unittest.main()
-    sys.exit()
-
-#-----Update data------
-
-data = {'納品日': "2020/11/02",'担当者': "小野",'摘要': "更新テスト"}
-j_data = json.dumps(data)
-ret = json_update(conn,'納品',j_data,'担当者')
-print (ret)
-
-#----Select data ------
-print("\n-----select from Table------")
-c.execute('SELECT * FROM 納品' )
-nous = c.fetchall()
-print (nous)
-
-#----Select data JSON Format------
-print("\n-----select data JSON ------")
-nous = json_select_all(conn,'納品')
-print(nous)
-
-#-----Delete data------
-print("\n-----Delete Table------")
-data = {'ID': 2}
-ret = dict_delete(conn,'納品',data,'ID')
-print (ret)
-
-data = {'ID': 1}
-j_data = json.dumps(data)
-ret = json_delete(conn,'納品',j_data,'ID')
-print (ret)
-
-#----Select data ------
-print("\n-----select from Table------")
-c.execute('SELECT * FROM 納品' )
-nous = c.fetchall()
-print (nous)
-
-conn.commit()
-conn.close()
